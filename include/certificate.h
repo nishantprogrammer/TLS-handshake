467143f69ab82204216e9cb254d8cafd5bf74fea
Nishant Dubey <nishantworkhub@example.com>
2025-03-31 14:03:59 +0530

    Added my file

---
+++ include/certificate.h
@@ -0,0 +1,43 @@
+
+#ifndef __CERTIFICATE_H__
+#define __CERTIFICATE_H__
+
+#define CA_CERTIFICATE_TAG 1
+#define SERVER_CERTIFICATE_TAG 2
+#define PSK_TAG 3
+
+
+#if defined(CONFIG_PSA_TLS_CERTIFICATE_TYPE_RSA)
+
+static const unsigned char ca_certificate[] = {
+#include "root-cert.der.inc"
+};
+static const unsigned char server_certificate[] = {
+#include "echo-apps-cert.der.inc"
+};
+/* This is the private key in pkcs#8 format. */
+static const unsigned char private_key[] = {
+#include "echo-apps-key.der.inc"
+};
+
+#elif defined(CONFIG_PSA_TLS_CERTIFICATE_TYPE_ECDSA)
+
+static const unsigned char ca_certificate[] = {
+#include "ec-root-cert.der.inc"
+};
+static const unsigned char server_certificate[] = {
+#include "ec-echo-apps-cert.der.inc"
+};
+/* This is the private key in pkcs#8 format. */
+static const unsigned char private_key[] = {
+#include "ec-echo-apps-key.der.inc"
+};
+
+#else
+
+#error "No certificate type selected"
+
+#endif
+
+
+#endif /* __CERTIFICATE_H__ */
---
+++ include/psa_tls_credentials.h
@@ -0,0 +1,29 @@
+
+#ifndef TLS_CREDENTIALS_H__
+#define TLS_CREDENTIALS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#define PSA_PS_CERTIFICATE_UID		(1)
+#define PSA_PS_CERTIFICATE_KEY_UID	(2)
+#define PSA_PS_CA_CERTIFICATE_UID	(3)
+
+#define APP_SUCCESS			(0)
+#define APP_ERROR			(-1)
+
+
+/** @brief Function for registering certificates and PSKs.
+ *
+ * @retval 0 TLS credential successfully set.
+ */
+int tls_set_credentials(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TLS_CREDENTIALS_H__ */
